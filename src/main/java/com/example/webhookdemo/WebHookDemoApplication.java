299a2e459f15adaaf79a360c90fd4d4a6f11f158
hyungkishin <tkaqkeldk@nate.com>
2023-04-12 23:44:08 +0900

    chore: 초기환경 구성

---
+++ src/main/java/com/example/webhookdemo/WebHookDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.webhookdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHookDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebHookDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webhookdemo/WebHookDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webhookdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHookDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
